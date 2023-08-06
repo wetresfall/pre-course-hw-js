d686615610f4a26772ba854be9eac4e1987cd922
Jack Belnik <jw.belnik@gmail.com>
2023-08-06 15:07:21 +0300

    fix lesson-02

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -2,8 +2,8 @@ function factorial(n) {
     if (n === 0 || n === 1) {
         return 1;
     } else {
+        debugger;
         return n * factorial(n - 1);
     }
 }
-
 console.log(factorial(5));
--- lesson-02/04.js
+++ lesson-02/04.js
@@ -1,9 +1,9 @@
 const giveMeRightCondition = (value1, value2, maximum) => {
-    if (value1 = value2 && value2 <= maximum) {
+    if (value1 === value2 && value2 <= maximum) {
         console.log('Success!');
     } else {
         console.log('Something wrong!');
     }
 }
 
-giveMeRightCondition(99, 100, 98)
+giveMeRightCondition(99, 100, 98);
