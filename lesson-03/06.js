83edd4be204e169bc3a09403cef077243f1d36a4
Jack Belnik <jw.belnik@gmail.com>
2023-08-07 23:32:40 +0300

    done lessons-03

--- lesson-03/06.js
+++ lesson-03/06.js
@@ -1,1 +1,38 @@
-// code
+const users3 = [
+  {
+      id: 1,
+      user: 'John',
+      surName: 'Doe',
+      address: '123 Main St'
+  },
+  {
+      id: 2,
+      user: 'Tom',
+      surName: 'Johnson',
+      address: '789 Oak St'
+  },
+  {
+      id: 3,
+      user: 'Bob',
+      surName: 'Jones',
+      address: '135 Pine St'
+  },
+  {
+      id: 4,
+      user: 'David',
+      surName: 'Davis',
+      address: '864 Walnut St'
+  },
+  {
+      id: 5,
+      user: 'Mark',
+      surName: 'Taylor',
+      address: '321 Hickory St'
+  },
+];
+
+users3.forEach(user => {
+  user.isMarried = true;
+});
+
+console.log(users3);
--- lesson-03/07.js
+++ lesson-03/07.js
@@ -1,1 +1,11 @@
-// code
+​const numbers = [
+  [1, 2, 3, 4, 5],
+  [11, 20, 33, 40, 55],
+  [111, 200, 333, 400, 555],
+];
+
+const filteredNumbers = numbers.map(row => {
+  return row.filter(num => num % 2 === 0);
+});
+
+console.log(filteredNumbers);
